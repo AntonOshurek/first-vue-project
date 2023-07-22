d2cf0c9043d39367f03b64c08f3c0f8d04f774af
Anton Oshurek <oshurekweb@gmail.com>
2023-07-22 15:12:51 +0200

    create basic items in login page

--- src/views/login/login.view.vue
+++ src/views/login/login.view.vue
@@ -1,16 +1,31 @@
 <template>
 	<div class="login-view">
 		<McvHeader></McvHeader>
+
+		<main class="login-view__main">
+			<div class="login-view__titles">
+				<h2 class="login-view__title">Sign in</h2>
+				<router-link class="register-view__link" to="/register">don't have an account?</router-link>
+			</div>
+
+			<McvValidationErrors v-if="validationErrors" :validation-errors="validationErrors" />
+		</main>
 	</div>
 </template>
 
 <script>
-import { McvHeader } from '@/components';
+import { McvHeader, McvValidationErrors } from '@/components';
 
 export default {
 	name: 'MvcLogin',
+	computed: {
+		validationErrors() {
+			return this.$store.state.auth.validationErrors;
+		},
+	},
 	components: {
 		McvHeader,
+		McvValidationErrors,
 	},
 };
 </script>
