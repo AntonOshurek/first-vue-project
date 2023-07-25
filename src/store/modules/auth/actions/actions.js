f051d7b5f9f2157fe0aadb8e945ad6435c0ee843
Anton Oshurek <oshurekweb@gmail.com>
2023-07-25 19:09:29 +0200

    split the auth state block into parts

---
+++ src/store/modules/auth/actions/actions.js
@@ -0,0 +1,58 @@
+import { actionTypes } from '@/store/action-types/action-types';
+import { mutationTypes } from '@/store/mutation-types/mutation-types';
+import * as authApi from '@/api/auth';
+import { setItem } from '@/helpers/persistanceStorage';
+
+const actions = {
+	[actionTypes.authRegister](context, credentials) {
+		context.commit(mutationTypes.authRegisterStart);
+
+		return new Promise((resolve, reject) => {
+			authApi
+				.register(credentials)
+				.then((res) => {
+					context.commit(mutationTypes.authRegisterSuccess, res.data.user);
+					setItem('accessToken', res.data.user.token);
+					resolve(res.data.user);
+				})
+				.catch((err) => {
+					context.commit(mutationTypes.authRegisterFailure, err.response.data.errors);
+					reject(err);
+				});
+		});
+	},
+	[actionTypes.authLogin](context, credentials) {
+		context.commit(mutationTypes.authLoginStart);
+
+		return new Promise((resolve, reject) => {
+			authApi
+				.login(credentials)
+				.then((res) => {
+					context.commit(mutationTypes.authLoginSuccess, res.data.user);
+					setItem('accessToken', res.data.user.token);
+					resolve(res.data.user);
+				})
+				.catch((err) => {
+					context.commit(mutationTypes.authLoginFailure, err.response.data.errors);
+					reject(err);
+				});
+		});
+	},
+	[actionTypes.getCurrentUser](context) {
+		context.commit(mutationTypes.getCurrentUserStart);
+
+		return new Promise((resolve) => {
+			authApi
+				.getCurrentUser()
+				.then((res) => {
+					context.commit(mutationTypes.getCurrentUserSuccess, res.data.user);
+					resolve(res.data.user);
+				})
+				.catch(() => {
+					context.commit(mutationTypes.getCurrentUserFailure);
+				});
+		});
+	},
+};
+
+export { actions };
---
+++ src/store/modules/auth/getters/getters.js
@@ -0,0 +1,9 @@
+import { getterTypes } from '@/store/getter-types/getter-types';
+
+export const getters = {
+	[getterTypes.currentUser]: (state) => state.currentUser,
+	[getterTypes.isSubmiting]: (state) => state.isSubmiting,
+	[getterTypes.isLoggedIn]: (state) => Boolean(state.isLoggedIn),
+	[getterTypes.isAnonymous]: (state) => state.isLoggedIn === false,
+	[getterTypes.validationErrors]: (state) => state.validationErrors,
+};
