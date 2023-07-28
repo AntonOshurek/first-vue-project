0e11a6e0d62a1e4d17f0b6b3eec7d8e76d791480
AntonOshurek <oshurekweb@gmail.com>
2023-07-28 19:15:09 +0200

    create styles for feed page

--- src/components/feed/feed.vue
+++ src/components/feed/feed.vue
@@ -1,58 +1,55 @@
 <template>
-	<div class="feed">
+	<section class="feed">
 		<h3 class="visually-hidden">feed overview</h3>
 
 		<div v-if="isLoading">Loading...</div>
 
 		<div v-if="error">Something bad happened.</div>
 
-		<section class="feed-overview" v-if="feedData">
-			<article class="feed-article" v-for="(article, index) in feedData.articles" :key="index">
-				<header class="feed-articel__header">
-					<p class="feed-article__username">{{ article.author.username }}</p>
-					<time class="feed-article__post-date" datetime="">
-						{{ article.createdAt }}
-					</time>
+		<ul class="feed-list" v-if="feedData">
+			<li class="feed-overview__item" v-for="(article, index) in feedData.articles" :key="index">
+				<article class="feed-article">
+					<header class="feed-articel__header">
+						<p class="feed-article__username">{{ article.author.username }}</p>
+						<time class="feed-article__post-date" datetime="">
+							{{ article.createdAt }}
+						</time>
+						<img class="feed-article__author-image" :src="article.author.image" alt="" />
 
-					<div class="feed-article__likes">like {{ article.favoritesCount }}</div>
-				</header>
+						<button class="feed-article__likes" type="button">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								height="20"
+								viewBox="0 -960 960 960"
+								width="20"
+							>
+								<path
+									d="M480-219.5 468-231q-95.133-86.184-157.067-146.092Q249-437 214.216-480.902q-34.785-43.901-48-78.473Q153-593.947 153-628.5q0-64.505 45.498-110.002Q243.995-784 308.5-784q49.466 0 93.983 27.5t77.517 81q33.5-53.5 77.75-81T651.5-784q64.505 0 110.002 45.443Q807-693.114 807-628.692q0 34.73-12.716 68.309-12.715 33.58-47.464 76.924-34.749 43.345-96.901 104.366Q587.766-318.072 490-229l-10 9.5Z"
+								/>
+							</svg>
+							{{ article.favoritesCount }}
+						</button>
+					</header>
 
-				<h4 class="feed-article__title">{{ article.title }}</h4>
-				<p class="feed-article__description">{{ article.description }}</p>
+					<h4 class="feed-article__title">{{ article.title }}</h4>
+					<p class="feed-article__description">{{ article.description }}</p>
 
-				<div class="feed-article__additionally">
-					<span class="feed-article__tags" v-for="(tag, index) in article.tagList" :key="index">
-						{{ tag }}
-					</span>
-					<a href="#">read more...</a>
-				</div>
-			</article>
-		</section>
-	</div>
+					<div class="feed-article__additionally">
+						<a class="feed-article__read-more" href="#">read more...</a>
+
+						<div class="feed-article__tags">
+							<span class="feed-article__tag" v-for="(tag, index) in article.tagList" :key="index">
+								{{ tag }}
+							</span>
+						</div>
+					</div>
+				</article>
+			</li>
+		</ul>
+	</section>
 </template>
 
 <!-- https://demo.realworld.io/#/ -->
-
-<!-- {
-	const author = {
-  bio: null,
-  following: false,
-  image: "https://api.realworld.io/images/demo-avatar.png",
-  username: "Anah Benešová",
-};
-
-const article = {
-  body: "Sunt excepturi ut dolore fuga. Autem eum maiores aut nihil magnam corporis consectetur sit. Voluptate et quasi optio eos et eveniet culpa et nobis. Sint aut sint sequi possimus reiciendis nisi. Rerum et omnis et sit doloribus corporis voluptas error. Iusto molestiae tenetur necessitatibus dolorem omnis. Libero sed ut architecto. Ex itaque et modi aut voluptatem alias quae. Modi dolor cupiditate sit. Delectus consectetur nobis aliquid deserunt sint ut et voluptas. Corrupti in labore laborum quod. Ipsa laudantium deserunt. Ut atque harum inventore natus facere sed molestiae. Quia aliquid ut. Animi sunt rem et sit ullam dolorem ab consequatur modi. Cupiditate officia voluptatum. Tenetur facere eum distinctio animi qui laboriosam. Quod sed voluptatem et cumque est eos. Sint id provident suscipit harum odio et. Et fuga repellendus magnam dignissimos eius aspernatur rerum. Quo perferendis nesciunt. Dolore dolorem porro omnis voluptatibus consequuntur et expedita suscipit et. Tempora facere ipsa. Dolore accusamus soluta officiis eligendi. Eum quaerat neque eum beatae odio. Ad voluptate vel. Aut aut dolor. Cupiditate officia voluptatum. Tenetur facere eum distinctio animi qui laboriosam. Quod sed voluptatem et cumque est eos. Sint id provident suscipit harum odio et.",
-  createdAt: "2022-12-09T13:46:24.264Z",
-  description: "Assumenda molestiae laboriosam enim ipsum quaerat enim officia vel quo. Earum odit rem natus totam atque cumque. Sint dolorem facere non.",
-  favorited: false,
-  favoritesCount: 1467,
-  slug: "Try-to-transmit-the-HTTP-card-maybe-it-will-override-the-multi-byte-hard-drive!-120863",
-  tagList: ["voluptate", "rerum", "ducimus", "hic"],
-  title: "Try to transmit the HTTP card, maybe it will override the multi-byte hard drive!",
-  updatedAt: "2022-12-09T13:46:24.264Z",
-};
-} -->
 
 <script>
 import { mapGetters } from 'vuex';
