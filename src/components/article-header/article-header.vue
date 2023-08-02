400a7288be6f4f7af7a75b9625984fe8566a331b
AntonOshurek <oshurekweb@gmail.com>
2023-08-02 19:44:30 +0200

    add display user controls only were he is logged in

--- src/components/article-header/article-header.vue
+++ src/components/article-header/article-header.vue
@@ -5,15 +5,17 @@
 		<div class="article-header__user-info">
 			<McvArticleAuthor :article-data="articleData" />
 
-			<McvCommonArticleControls :article-data="articleData" />
+			<McvCommonArticleControls v-if="!isAuthor" :article-data="articleData" />
 
-			<McvUserArticleControls :article-data="articleData" />
+			<McvUserArticleControls v-if="isAuthor" :article-data="articleData" />
 		</div>
 	</div>
 </template>
 
 <script>
 import { routesNames } from '@/variables/rotes';
+import { mapGetters } from 'vuex';
+import { getterTypes } from '@/store/getter-types/getter-types';
 import McvArticleAuthor from '@/components/article-author/article-author';
 import McvCommonArticleControls from '@/components/article-controls/common-article-controls/common-article-controls';
 import McvUserArticleControls from '@/components/article-controls/user-article-controls/user-article-controls';
@@ -27,8 +29,19 @@ export default {
 		},
 	},
 	computed: {
+		...mapGetters({
+			currentUser: getterTypes.currentUser,
+			isLoggedIn: getterTypes.isLoggedIn,
+		}),
 		routesNames() {
 			return routesNames;
+		},
+		isAuthor() {
+			if (!this.currentUser || !this.articleData || !this.isLoggedIn) {
+				return false;
+			}
+
+			return this.currentUser.username === this.articleData.author.username;
 		},
 	},
 	components: {
